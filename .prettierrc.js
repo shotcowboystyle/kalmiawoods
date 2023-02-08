2f38e10a91553cc437455b848804c6cd1d6f173a
Curtis Blanton <62471+shotcowboystyle@users.noreply.github.com>
2023-02-08 13:46:29 -0500

    ci(tools): add various code tooling/helpers

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  "tabWidth": 2,
+  "useTabs": false,
+  "printWidth": 120,
+  "singleQuote": true,
+  "trailingComma": "all",
+  "semi": true,
+  "jsxBracketSameLine": false
+};
