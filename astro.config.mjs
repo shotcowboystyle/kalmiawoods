9111477f2f03b3298e6c3dce006fc12b3374e985
Curtis Blanton <62471+shotcowboystyle@users.noreply.github.com>
2023-07-12 17:12:45 -0400

    fix: revert code changes fixing deployment errors

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,24 @@
-// import image from '@astrojs/image';
 import mdx from '@astrojs/mdx';
 import partytown from '@astrojs/partytown';
-// import prefetch from '@astrojs/prefetch';
+import prefetch from '@astrojs/prefetch';
 import tailwind from '@astrojs/tailwind';
 import vercel from '@astrojs/vercel/serverless';
 import vue from '@astrojs/vue';
-// import AstroPWA from '@vite-pwa/astro';
-// import compress from 'astro-compress';
-// import critters from 'astro-critters';
+import compress from 'astro-compress';
+import critters from 'astro-critters';
 import devOnlyRoutes from 'astro-dev-only-routes';
 import icon from 'astro-icon';
-import { defineConfig } from 'astro/config';
+import { defineConfig, sharpImageService } from 'astro/config';
 import AutoImport from 'unplugin-auto-import/astro';
 import IconsResolver from 'unplugin-icons/resolver';
 import Icons from 'unplugin-icons/vite';
-// import { HeadlessUiResolver } from 'unplugin-vue-components/resolvers';
 import Components from 'unplugin-vue-components/vite';
 import { loadEnv } from 'vite';
 import mkcert from 'vite-plugin-mkcert';
 
-const { APP_HOST, APP_PORT, APP_SITE, APP_BASE } = loadEnv(process.env.MODE, process.cwd(), '');
+const { APP_SITE, APP_BASE } = loadEnv(process.env.MODE, process.cwd(), '');
 const basePath = `${(APP_BASE ?? '/').replace(/\/$/, '')}/`;
-// const __dirname = dirname(fileURLToPath(import.meta.url));
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
@@ -30,22 +27,16 @@ export default defineConfig({
     analytics: true,
     // imageService: true,
   }),
-  // experimental: {
-  //   assets: true,
-  // },
+  experimental: {
+    assets: true,
+  },
   // build: {
   //   excludeMiddleware: false,
-  // },
-  // server: {
-  //   host: APP_HOST,
-  //   port: +APP_PORT,
+  //   split: true,
   // },
   site: APP_SITE,
   base: basePath,
   trailingSlash: 'never',
-  // build: {
-  //   split: true,
-  // },
   integrations: [
     vue({}),
     tailwind({
@@ -62,39 +53,9 @@ export default defineConfig({
         'icon-park-outline': ['game-ps', 'camp'],
       },
     }),
-    // AstroPWA({
-    //   registerType: 'autoUpdate',
-    //   base: basePath,
-    //   includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
-    //   manifest: {
-    //     name: 'Manila Observatory - Panahon',
-    //     short_name: 'MO - Panahon',
-    //     background_color: '#ffffff',
-    //     description: 'Manila Observatory - Panahon website',
-    //     theme_color: '#ffffff',
-    //     icons: [
-    //       {
-    //         src: `${basePath}resources/static/img/logo/android-chrome-192x192.png`,
-    //         sizes: '192x192',
-    //         type: 'image/png',
-    //       },
-    //       {
-    //         src: `${basePath}resources/static/img/logo/android-chrome-512x512.png`,
-    //         sizes: '512x512',
-    //         type: 'image/png',
-    //       },
-    //       {
-    //         src: `${basePath}resources/static/img/logo/android-chrome-512x512.png`,
-    //         sizes: '512x512',
-    //         type: 'image/png',
-    //         purpose: 'any maskable',
-    //       },
-    //     ],
-    //   },
-    // }),
-    // image({
-    //   serviceEntryPoint: '@astrojs/image/sharp',
-    // }),
+    image({
+      service: sharpImageService(),
+    }),
     mdx(),
     partytown({
       config: {
@@ -112,18 +73,18 @@ export default defineConfig({
       dirs: ['src/composables'],
       vueTemplate: true,
     }),
-    // prefetch(),
-    // critters(),
-    // compress({
-    //   css: false,
-    //   html: {
-    //     removeAttributeQuotes: false,
-    //   },
-    //   img: false,
-    //   js: true,
-    //   svg: false,
-    //   logger: 1,
-    // }),
+    prefetch(),
+    critters(),
+    compress({
+      css: false,
+      html: {
+        removeAttributeQuotes: false,
+      },
+      img: false,
+      js: true,
+      svg: false,
+      logger: 1,
+    }),
     devOnlyRoutes(),
   ],
   markdown: {},
@@ -134,24 +95,15 @@ export default defineConfig({
     // ssr: {
     //   external: ['svgo'],
     // },
-    // define: {
-    //   __DATE__: `'${new Date().toISOString()}'`,
-    // },
+    define: {
+      __DATE__: `'${new Date().toISOString()}'`,
+    },
     server: {
-      // open: true,
       https: true,
     },
     plugins: [
       Components({
-        resolvers: [
-          // HeadlessUiResolver(),
-          IconsResolver(),
-          // (componentName) => {
-          //   // where `componentName` is always CapitalCase
-          //   if (componentName.startsWith('VueScheduler'))
-          //     return { name: componentName.slice(3), from: 'v-calendar-scheduler' };
-          // },
-        ],
+        resolvers: [IconsResolver()],
 
         dts: 'src/components.d.ts',
         directoryAsNamespace: true,
@@ -165,10 +117,10 @@ export default defineConfig({
     optimizeDeps: {
       include: ['vue', '@vueuse/core', 'v-calendar'],
     },
-    // resolve: {
-    //   alias: {
-    //     '@': resolve(__dirname, './src'),
-    //   },
-    // },
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src'),
+      },
+    },
   },
 });
