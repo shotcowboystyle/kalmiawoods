f543b9738cad8a8b20fab6bb945134e642bc8d11
Curtis Blanton <62471+shotcowboystyle@users.noreply.github.com>
2023-11-03 17:30:40 -0400

    fix: disable vercel image service

--- astro.config.mjs
+++ astro.config.mjs
@@ -64,10 +64,10 @@ export default defineConfig({
 		speedInsights: {
 			enabled: true,
 		},
-		imagesConfig: {
-			sizes: [320, 640, 1280, 1920],
-		},
-		imageService: true,
+		// imagesConfig: {
+		// 	sizes: [320, 640, 1280, 1920],
+		// },
+		imageService: false,
 		edgeMiddleware: false,
 	}),
 	integrations: [
