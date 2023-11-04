41d0988f18eb34ab2382bd49c2f76e3971927124
Curtis Blanton <62471+shotcowboystyle@users.noreply.github.com>
2023-11-04 11:54:14 -0400

    fix: vercel image optimizations

--- astro.config.mjs
+++ astro.config.mjs
@@ -66,13 +66,15 @@ export default defineConfig({
 		},
 		imagesConfig: {
 			sizes: [640, 750, 828, 1080, 1200],
-			minimumCacheTTL: 60,
 			formats: ['image/avif', 'image/webp'],
 		},
 		imageService: true,
 		devImageService: 'sharp',
 		edgeMiddleware: false,
 	}),
+	image: {
+		domains: [],
+	},
 	integrations: [
 		svgSprite({
 			mode: 'verbose',
