c9dde02586c57a3b36ba6503ec75a61a23efdf52
Curtis Blanton <62471+shotcowboystyle@users.noreply.github.com>
2023-11-05 07:43:53 -0500

    fix: domains array in vercel imagesConfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -67,6 +67,7 @@ export default defineConfig({
 		imagesConfig: {
 			sizes: [640, 750, 828, 1080, 1200],
 			formats: ['image/avif', 'image/webp'],
+			domains: [],
 		},
 		imageService: true,
 		devImageService: 'sharp',
